7621e572c86490b965a24b8d489d39e7309698e7
Nikolay Gromin <gromin@gmail.com>
2017-07-24 07:21:54 +0300

    Test webscokets on ./client/: requests and subscriptions

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Nes = require('nes');
+
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      greeting: null
+      greeting: null,
+      socketGreeting: null
     }
+    this.onSocketUpdate = this.onSocketUpdate.bind(this);
   }
 
   componentDidMount() {
-    fetch('/me')
+    this.socketClient = new Nes.Client('ws://localhost:3000/ws');
+
+    this.socketClient
+        .connect((err) => {
+          this.socketClient.request('/ws', (err, payload) => {
+            console.log('socket', payload);
+            this.setState({socketGreeting: payload});
+          });
+        });
+
+    this.socketClient
+        .subscribe('/ws/machines',
+                   this.onSocketUpdate,
+                   this.onSocketSubscribe);
+
+    fetch('/api/greet/user')
       .then(response => response.text())
       .then(response => this.setState({greeting: response}));
+  }
+
+  onSocketSubscribe(err) {}
+
+  onSocketUpdate(update, flags) {
+    console.log(update);
   }
 
   greeting() {
@@ -21,6 +46,19 @@ class App extends Component {
       return (
         <p className="App-intro">
           {this.state.greeting}
+        </p>
+      );
+    }
+    else {
+      return null;
+    }
+  }
+
+  socketGreeting() {
+    if (this.state.socketGreeting) {
+      return (
+        <p className="App-intro">
+          {this.state.socketGreeting}
         </p>
       );
     }
@@ -37,6 +75,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         { this.greeting() }
+        { this.socketGreeting() }
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
