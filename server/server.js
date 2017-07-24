9f51a5e40a256353c4ded3dc920b42a169974723
Nikolay Gromin <gromin@gmail.com>
2017-07-24 07:19:36 +0300

    Add to ./server/ simple machines list service with faked data and websocket subscriptions

--- server/server.js
+++ server/server.js
@@ -1,30 +1,113 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Nes = require('nes');
+
+const MachinesDb = require('./services/machines_db');
 
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: '0.0.0.0' });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
+server.register([Nes], function(err) {
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
+    server.subscription('/ws/machines');
 
-server.start((err) => {
+    server.route({
+        method: 'GET',
+        path: '/api/greet',
+        handler: function (request, reply) {
+            reply('Hello, world!');
+        }
+    });
 
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
+    server.route({
+        method: 'GET',
+        path: '/api/greet/{name}',
+        handler: function (request, reply) {
+            reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/machines',
+        handler: function (request, reply) {
+            reply(server.machines.all());
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/api/machines',
+        handler: function (request, reply) {
+            const machineSpec = JSON.parse(request.payload);
+            const machine = server.machines.addMachine(machineSpec);
+            if (machine) {
+                reply(machine);
+            }
+            else {
+                reply('').code(204);
+            }
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/api/machines/{id}',
+        handler: function (request, reply) {
+            const machine = server.machines.getMachine(request.params.id);
+            if (machine) {
+                reply(machine);
+            } else {
+                reply('').code(404);
+            }
+        }
+    });
+
+    server.route({
+        method: 'PUT',
+        path: '/api/machines/{id}',
+        handler: function (request, reply) {
+            const id = request.params.id;
+            const machineSpec = JSON.parse(request.payload);
+
+            let machine = server.machines.getMachine(id);
+            if (!machine) {
+                reply('').code(404);
+                return;
+            }
+            machine = server.machines.updateMachine(id, machineSpec);
+            reply(machine);
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/ws',
+        config: {
+            id: 'websocket',
+            handler: function(request, reply) {
+                return reply('Hello from websocket');
+            }
+        }
+    });
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        console.log(`Server running at: ${server.info.uri}`);
+
+        const machines = new MachinesDb(10);
+        server.machines = machines;
+
+        machines.on('add', (machine) => {
+            server.publish('/ws/machines', machine);
+        });
+
+        machines.on('update', (machine) => {
+            server.publish('/ws/machines', machine);
+        });
+    });
 });
---
+++ server/services/machines_db.js
@@ -0,0 +1,59 @@
+const EventEmitter = require('events');
+const faker = require('faker');
+
+function genFakeMachine(index) {
+  index = index || 0;
+  faker.locale = 'ru';
+  return {
+    ip: faker.internet.ip(),
+    name: `dev-${index}`,
+    owner: faker.name.findName(),
+    isOnline: faker.random.boolean()
+  }
+}
+
+class MachinesDb extends EventEmitter {
+
+  constructor(genOnStart) {
+    super()
+
+    genOnStart = genOnStart || 0;
+
+    this.machines = {};
+
+    let i = 0;
+    while (i++ < genOnStart) {
+      this.addMachine(genFakeMachine(i));
+    }
+  }
+
+  static genUuid(machine) {
+    return faker.random.uuid().substr(0, 8);
+  }
+
+  all() {
+    return this.machines;
+  }
+
+  getMachine(id) {
+    return this.machines[id];
+  }
+
+  addMachine(machine) {
+    const id = MachinesDb.genUuid();
+    machine.id = id;
+
+    this.machines[id] = machine;
+    setImmediate(() => this.emit('add', this.machines[id]));
+  }
+
+  updateMachine(id, machineSpec) {
+    if (!this.machines[id]) {
+      return null;
+    }
+    this.machines[id] = Object.assign(this.machines[id], machineSpec);
+    setImmediate(() => this.emit('update', this.machines[id]));
+  }
+}
+
+module.exports = MachinesDb;
