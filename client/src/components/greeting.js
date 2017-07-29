b08177eb9169669064e98defe6f5b34c88d50f6b
Nikolay Gromin <gromin@gmail.com>
2017-07-29 14:38:57 +0300

    Greetings use Alerts from bootstrap

--- client/src/components/greeting.js
+++ client/src/components/greeting.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
 
 class Greeting extends Component {
   render() {
     return (
-      <p className="App-intro">
+      <Alert bsStyle="success">
         {this.props.text}
-      </p>
+      </Alert>
     )
   }
 }
